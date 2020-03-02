d966f04b4ffe4f3653da8389da3f0f1c30a68616
Connor Shaughnessy <sapphon@gmail.com>
2020-03-02 14:32:18 -0500

    Initial source (unchanged from start.spring.io)

---
+++ src/main/java/org/sapphon/foiltray/FoilTrayApplication.java
@@ -0,0 +1,13 @@
+package org.sapphon.foiltray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoilTrayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoilTrayApplication.class, args);
+	}
+
+}
