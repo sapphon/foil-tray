8535d31d472f0101a53b984394b84ea169c391be
Sapphon <sapphon@gmail.com>
2021-03-08 23:49:52 -0500

    Default AnimationMotions are now added if none exist when API starts [https://trello.com/c/cMe6bNCn]

--- src/main/java/org/sapphon/foiltray/controller/AnimationMotionController.java
+++ src/main/java/org/sapphon/foiltray/controller/AnimationMotionController.java
@@ -1,5 +1,6 @@
 package org.sapphon.foiltray.controller;
 
+import lombok.Data;
 import org.sapphon.foiltray.model.AnimationMotion;
 import org.sapphon.foiltray.repository.AnimationMotionRepository;
 import org.springframework.http.HttpStatus;
@@ -10,13 +11,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @Controller
 public class AnimationMotionController {
 
     private final AnimationMotionRepository animationMotionRepository;
 
+    @Data
+    private class Motions{
+        List<AnimationMotion> animationMotions;
+        public Motions(List<AnimationMotion> motions){
+            this.animationMotions = motions;
+        }
+    }
+
     public AnimationMotionController(AnimationMotionRepository animationMotionRepository) {
         this.animationMotionRepository = animationMotionRepository;
+        if(animationMotionRepository.count() == 0){
+            addDefaultMotions(animationMotionRepository);
+        }
+    }
+
+    private void addDefaultMotions(AnimationMotionRepository repository) {
+        repository.save(new AnimationMotion("Idle"));
+        repository.save(new AnimationMotion("Walk"));
+        repository.save(new AnimationMotion("Jump"));
+        repository.save(new AnimationMotion("Get Item"));
+        repository.save(new AnimationMotion("Use Item"));
+        repository.save(new AnimationMotion("Talk"));
+        repository.save(new AnimationMotion("Hit"));
+        repository.save(new AnimationMotion("Shoot"));
     }
 
     @GetMapping("/api/v1/motion/{motionName}")
@@ -29,5 +57,12 @@ public class AnimationMotionController {
     public ResponseEntity postMotion(@RequestBody AnimationMotion incomingMotion) {
         AnimationMotion found = animationMotionRepository.findByName(incomingMotion.getName());
         return found == null ? new ResponseEntity<>(animationMotionRepository.save(incomingMotion), HttpStatus.OK) : new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping("api/v1/motions")
+    public ResponseEntity<Motions> getMotions(){
+        return new ResponseEntity<>(new Motions(StreamSupport
+                .stream(animationMotionRepository.findAll().spliterator(), false)
+                .collect(Collectors.toList())), HttpStatus.OK);
     }
 }
--- src/test/java/org/sapphon/foiltray/FoilTrayApplicationTests.java
+++ src/test/java/org/sapphon/foiltray/FoilTrayApplicationTests.java
@@ -1,12 +1,31 @@
 package org.sapphon.foiltray;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FoilTrayApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void defaultMotionsExist() throws Exception {
+        mockMvc.perform(get("/api/v1/motions")).andExpect(status().isOk()).andExpect(content().json("{" +
+                "\"animationMotions\":[{\"id\":1,\"name\":\"Idle\"},{\"id\":2,\"name\":\"Walk\"},{\"id\":3,\"name\":\"Jump\"},{\"id\":4,\"name\":\"Get Item\"},{\"id\":5,\"name\":\"Use Item\"},{\"id\":6,\"name\":\"Talk\"},{\"id\":7,\"name\":\"Hit\"},{\"id\":8,\"name\":\"Shoot\"}]" +
+                "}"));
     }
 
 }
